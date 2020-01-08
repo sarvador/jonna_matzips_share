5645de0724aa402f57bcbee963c08a93bb0e3d96
Jin Wook <kry330@naver.com>
2020-01-08 10:26:21 +0900

    url&templates setting finish

--- jonna_matzips_share/shareRes/views.py
+++ jonna_matzips_share/shareRes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
 
 # Create your views here.
+
+def index(request):
+    #return HttpResponse("index")
+    return render(request, 'shareRes/index.html')
+def restaurantDetail(requset):
+    #return HttpResponse("restaurantDetail")
+    return render(request, 'shareRes/restaurantDetail.html')
+    
+def restaurantCreate(requset):
+    #return HttpResponse("restaurantCreate")
+    return render(request, 'shareRes/restaurantCreate.html')
+    
+def categoryCreate(requset):
+    #return HttpResponse("categoryCreate")
+    return render(request, 'shareRes/categoryCreate.html')
